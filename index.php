015f6c6c33f4d541f4c76ceb34d9cc2d3f2144b0
GauthierBosson <32874699+GauthierBosson@users.noreply.github.com>
2018-01-09 13:08:35 +0100

    Ajout feature callpage
    Ajout des fichiers pour callpage

--- index.php
+++ index.php
@@ -12,27 +12,8 @@
 <div id="container">
 <?php include_once("./include/header.php");?>
 <main>
-<?php
-if (isset($_GET['page']) && $_GET['page'] != "") {
-    $page = $_GET['page'];    
-}
-
-else {
-    $page = "default";
-}
-
-$page = "./include/" . $page . ".inc.php";
-
-$incFiles = glob("./include/*.inc.php");
-
-if(in_array($page, $incFiles)) {
-    include($page);
-}
-
-else {
-    include("./include/default.inc.php");
-}
-
+    <?php
+include_once("./functions/CallPage.php");
 ?>
 </main>
 <?php include_once("./include/footer.php");
