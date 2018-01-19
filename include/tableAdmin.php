3a36e6307827a62ff05270804d8c6aa8c1a99ee5
GauthierBosson <32874699+GauthierBosson@users.noreply.github.com>
2018-01-19 16:06:53 +0100

    Modif page admin

--- include/tableAdmin.php
+++ include/tableAdmin.php
@@ -1,22 +1,23 @@
-<table>
-    <tr>
-        <th>Nom</th>
-        <th>Prenom</th>
-        <th>Mail</th>
-        <th>Roles</th>
-    </tr>
-    <?php
-    $connexion = mysqli_connect("localhost", "root", "", "nfactoryblog");
-    $requete = "SELECT * FROM t_users ";
-    if($result = mysqli_query($connexion, $requete)) {
-        if (mysqli_num_rows($result) > 0) {
-            while ($donnees = mysqli_fetch_array($result)) {
-                echo("<tr><td>" . $donnees['USERNAME'] . "</td>" . "<td>" . $donnees['USERFNAME'] . "</td>" . "<td>" . $donnees['USERMAIL'] . "</td>" . "<td>" . $donnees['T_ROLES_ID_ROLE'] . "</td>" . "</tr>");
-            }
-        } else
-            //$_SESSION['login'] = 0;
-            echo("Votre e-mail ou mot de passe est érronné");
-    }
-    ?>
+<?php
+if($_SESSION['admin']==1) {
+    $connexion=mysqli_connect("localhost","root","","NFactoryBlog");
+    $requete = "SELECT * FROM t_users";
+    $result= mysqli_query($connexion,$requete);
+    echo"<table>";
+    while ($donnees=mysqli_fetch_array($result)){
+        echo("<tr><td>".$donnees['ID_USER']."</td>"."<td>".$donnees['USERNAME']."</td>"."<td>".$donnees['USERFNAME']."</td>"."<td>".$donnees['USERMAIL']
+            ."</td>"."<td>".$donnees['T_ROLES_ID_ROLE']."</td>"."<td>"."<form method='post' action='#'>"."<select name='select'><option value='1'>1</option>
+            <option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option>
+            </select> "."</td>"."<td>"."<input type='text' name='id'>"."<input type='submit' value='Mettre a jour' name='formulaire'>"."</form>"."</td>"."</tr>");
 
-</table>
+    }
+    echo("</table>");
+    if(isset($_POST['formulaire'])){
+        $id=$_POST['id'];
+        $choix=$_POST['select'];
+        $requete2= "UPDATE t_users SET T_ROLES_ID_ROLE='$choix' WHERE ID_USER='$id'";
+        mysqli_query($connexion,$requete2);
+    }
+} else {
+    echo "erreur";
+}
